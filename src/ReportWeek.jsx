07d12facac3dba94720a7624ddd73477507acc83
Yujeong Lee <124586036+L-Y-Jeong@users.noreply.github.com>
2024-05-25 23:40:44 +0900

    Create ReportWeek.jsx

---
+++ src/ReportWeek.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ReportWeek.css';
+
+function ReportWeek() {
+  return (
+    <div className="report-container">
+      <div className="weekly-chart">
+        <div className="chart-placeholder">
+          {/* Add chart component or placeholder here */}
+          <p>Weekly Report Chart</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ReportWeek;
