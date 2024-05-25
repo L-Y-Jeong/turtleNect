d930af4504f7df646642bc79da51e11b54429254
Yujeong Lee <124586036+L-Y-Jeong@users.noreply.github.com>
2024-05-25 23:35:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 import Signup from './Signup';
 import Myinfo from './Myinfo';
+import ReportToday from './ReportToday';
+import Layout from './Layout';
 
 function App() {
   return (
-      <div className="App">
-        <Routes>
-          {/* 로그인 페이지 */}
-          <Route path="/login" element={<Login />} />
-          {/* 홈 페이지 */}
-          <Route path="/home" element={<Home />} />
-          {/* 회원가입 페이지*/}
-          <Route path="/signup" element={<Signup />} />
-          {/* 내 정보 페이지 */}
-          <Route path="/myinfo" element={<Myinfo />} />
-           {/* 모든 경로를 로그인 페이지로 리디렉션 */}
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </div>
+    <div className="App">
+      <Routes>
+        {/* 로그인 페이지 */}
+        <Route path="/login" element={<Login />} />
+        {/* 회원가입 페이지 */}
+        <Route path="/signup" element={<Signup />} />
+        {/* 기본 경로를 로그인 화면으로 리디렉션 */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        {/* 보호된 라우트를 포함한 페이지 */}
+        <Route path="/app/*" element={<Layout />}>
+          <Route index element={<Navigate to="home" />} /> {/* 기본 경로를 홈으로 리디렉션 */}
+          <Route path="home" element={<Home />} />
+          <Route path="reportToday" element={<ReportToday />} />
+        </Route>
+        <Route path="myinfo" element={<Myinfo />} />
+        {/* 모든 경로를 로그인 페이지로 리디렉션 */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
   );
 }
 
